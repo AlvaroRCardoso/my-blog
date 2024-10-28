7349caee70660e5445ddedd1717bb6001df26250
Álvaro <alvaro.cardoso@universo.univates.br>
2024-10-28 19:08:32 -0300

    commit

---
+++ src/data/projects.js
@@ -0,0 +1,40 @@
+const projects = [
+  {
+    id: 1,
+    name: "Sollar Sul",
+    src: "/site-sollar.webp",
+    link: "https://www.sollarsul.com.br",
+    description:
+      "Esse é o primeiro site que desenvolvi foi para a empresa onde trabalho até hoje, criando aplicativos e sistemas mais complexos, embora esses trabalhos não sejam públicos. Esse projeto foi extremamente importante para minha carreira, pois representou minha primeira oportunidade como programador. Ele me permitiu demonstrar minhas habilidades e abrir caminho para desafios maiores e mais avançados no campo da tecnologia.",
+    description_en:
+      "This is the first website I developed for the company where I still work today, creating more complex applications and systems, although these projects are not public. This project was extremely important for my career, as it represented my first opportunity as a programmer. It allowed me to showcase my skills and paved the way for greater and more advanced challenges in the field of technology.",
+    date: "Mai 22, 2023",
+    date_en: "May 22, 2023",
+  },
+  {
+    id: 2,
+    name: "Hard Work",
+    src: "/site-hw.webp",
+    link: "https://hardwork.imb.br/",
+    description:
+      "Esse é um dos últimos sites que desenvolvi, criado com o objetivo de apresentar um novo complexo de lojas. Utilizando tecnologias como HTML, CSS, e JavaScript, fiz o meu melhor como programador para melhorar a experiência do usuário, garantindo uma navegação fluida e intuitiva, além de ser totalmente otimizado para dispositivos móveis. Esse projeto é um exemplo do meu compromisso em entregar soluções digitais de alta qualidade, alinhadas às necessidades e objetivos do cliente.",
+    description_en:
+      "This is one of the latest websites I developed, created with the goal of showcasing a new shopping complex. Using technologies like HTML, CSS, and JavaScript, I did my best as a programmer to enhance the user experience, ensuring smooth and intuitive navigation, while being fully optimized for mobile devices. This project is an example of my commitment to delivering high-quality digital solutions, aligned with the client's needs and objectives.",
+    date: "Jul 18, 2024",
+    date_en: "July 18, 2024",
+  },
+  {
+    id: 3,
+    name: "Grano Grãos e Sabores",
+    src: "/site-grano.webp",
+    link: "https://granograos.com.br/",
+    description:
+      "Como freelancer, desenvolvi um site especial para comemorar o aniversário da Grano, com uma funcionalidade interativa onde os clientes podiam escanear um QR code, acessar o site e girar uma roleta para ganhar brindes exclusivos. Esse recurso foi criado para tornar a experiência do aniversário mais envolvente. Embora a roleta tenha sido desativada após o evento, mantive o site no ar com informações sobre a loja e seus produtos.",
+    description_en:
+      "As a freelancer, I developed a special website to celebrate Grano's anniversary, featuring an interactive functionality where customers could scan a QR code, access the site, and spin a wheel to win exclusive gifts. This feature was designed to make the anniversary experience more engaging. Although the wheel has been disabled after the event, I kept the site online with information about the store and its products.",
+    date: "Out 25, 2024",
+    date_en: "Oct 25, 2024",
+  },
+];
+
+export default projects;
